38d2d92c8d0e688a29180698d0409c56231d98fc
Sanysak <sanysaklucky@gmail.com>
2019-03-29 12:09:02 +0700

    1.1

---
+++ data.js
@@ -0,0 +1,435 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pine-view-kitchen",
+      "name": "Pine View Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 0.3268595844965514,
+        "pitch": -0.2199823919007926,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.052626091097001,
+          "pitch": -0.025050521588385166,
+          "rotation": 0,
+          "target": "1-piproom"
+        },
+        {
+          "yaw": -2.11935417281728,
+          "pitch": -0.03924165627906184,
+          "rotation": 0,
+          "target": "2-boreyr-outside"
+        },
+        {
+          "yaw": -0.8054480853008954,
+          "pitch": -0.08289032607078006,
+          "rotation": 0,
+          "target": "6-boreya-upstair"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.26912164235212543,
+          "pitch": -0.027579026523152805,
+          "title": "Pine view kitchen",
+          "text": "<div class=\"info-hotspot-title\">We can taste the delicious food and relax with view around the restaurant.<br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-piproom",
+      "name": "Piproom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 0.38020739348781873,
+        "pitch": 0.06828916626061243,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8281992269798177,
+          "pitch": 0.06567915366757049,
+          "rotation": 0,
+          "target": "0-pine-view-kitchen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.17124444589371102,
+          "pitch": 0.23121013786242983,
+          "title": "Piproom",
+          "text": "The place we can relax and enjoy the nature."
+        }
+      ]
+    },
+    {
+      "id": "2-boreyr-outside",
+      "name": "BoreyR Outside",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -1.600979836052403,
+        "pitch": 0.03885177378078275,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6490431578756777,
+          "pitch": -0.006137662714099434,
+          "rotation": 0,
+          "target": "3-boreyr-view1"
+        },
+        {
+          "yaw": -0.6822717691442772,
+          "pitch": -0.0664977964820288,
+          "rotation": 0,
+          "target": "0-pine-view-kitchen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2428092437685905,
+          "pitch": -0.04724370483540952,
+          "title": "Borey R",
+          "text": "The place we can relax and enjoy the nature."
+        }
+      ]
+    },
+    {
+      "id": "3-boreyr-view1",
+      "name": "BoreyR View1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 2.377807849039579,
+        "pitch": 0.4781928123248065,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0000717455266255,
+          "pitch": -0.027323908928504892,
+          "rotation": 0,
+          "target": "5-boreyr-toilet"
+        },
+        {
+          "yaw": -1.5879478636932376,
+          "pitch": 0.13069425027827997,
+          "rotation": 0,
+          "target": "4-boreyr-view2"
+        },
+        {
+          "yaw": -0.877567087078285,
+          "pitch": 0.0332204946299921,
+          "rotation": 0,
+          "target": "0-pine-view-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-boreyr-view2",
+      "name": "BoreyR View2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 0.05534016194360092,
+        "pitch": 0,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6299293355457998,
+          "pitch": -0.010854328001279612,
+          "rotation": 0,
+          "target": "0-pine-view-kitchen"
+        },
+        {
+          "yaw": 0.6390162357796179,
+          "pitch": 0.24557123942646797,
+          "rotation": 0,
+          "target": "3-boreyr-view1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-boreyr-toilet",
+      "name": "BoreyR Toilet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 3.0654013560844753,
+        "pitch": 0.03717253571298684,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23011028357094254,
+          "pitch": 0.07786597811865903,
+          "rotation": 0,
+          "target": "3-boreyr-view1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-boreya-upstair",
+      "name": "BoreyA Upstair",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -2.216410975231277,
+        "pitch": 0.24673700713196212,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.33847966975232424,
+          "pitch": 0.439122450060232,
+          "rotation": 0,
+          "target": "7-boreya-room1"
+        },
+        {
+          "yaw": -1.6916733530102626,
+          "pitch": -0.049670364241352516,
+          "rotation": 0,
+          "target": "0-pine-view-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-boreya-room1",
+      "name": "BoreyA Room1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -0.468428328070047,
+        "pitch": 0.2407915370805931,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6410139101563619,
+          "pitch": 0.04993268686776098,
+          "rotation": 0,
+          "target": "8-boreya-room2"
+        },
+        {
+          "yaw": 0.4458937318122995,
+          "pitch": -0.16934086289769645,
+          "rotation": 0,
+          "target": "0-pine-view-kitchen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5026209114680977,
+          "pitch": 0.34812055579063994,
+          "title": "Borey A",
+          "text": "The place we can relax and enjoy the nature."
+        }
+      ]
+    },
+    {
+      "id": "8-boreya-room2",
+      "name": "BoreyA Room2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 1.5483709372299428,
+        "pitch": 0.2801541555347278,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6524086670859752,
+          "pitch": 0.07686067062854107,
+          "rotation": 0,
+          "target": "7-boreya-room1"
+        },
+        {
+          "yaw": 2.9496607580546286,
+          "pitch": -0.00591485354389043,
+          "rotation": 0,
+          "target": "0-pine-view-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
